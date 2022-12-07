3d3da6bf34ac6abce80f5a7ac006f321576720a1
Jamilu Salisu <jamilusalis@gmail.com>
2022-12-07 10:51:57 +0100

    add default password for DB connection

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,9 @@
 <?php 
 
-$conn= new mysqli('localhost:8888','root','','scheduling_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','root','scheduling_db')or die("Could not connect to mysql".mysqli_error($con));
+
+// if (mysqli_connect_error()) {
+//     die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
+//   }
+
+//   echo 'Connected successfully.';
