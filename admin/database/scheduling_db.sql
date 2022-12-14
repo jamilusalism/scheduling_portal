ead23881100449e5cf52bfbfe5b687e530efcb17
Jamilu Salisu <jamilusalis@gmail.com>
2022-12-14 18:02:36 +0100

    db file

--- admin/database/scheduling_db.sql
+++ admin/database/scheduling_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 20, 2020 at 11:01 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Host: localhost:8889
+-- Generation Time: Dec 14, 2022 at 05:00 PM
+-- Server version: 5.7.34
+-- PHP Version: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,19 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `class_schedule_info`
---
-
-CREATE TABLE `class_schedule_info` (
-  `id` int(30) NOT NULL,
-  `schedule_id` int(30) NOT NULL,
-  `course_id` int(30) NOT NULL,
-  `subject` int(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `courses`
 --
 
@@ -51,10 +38,9 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`id`, `course`, `description`) VALUES
-(1, 'Information Technology', 'IT'),
-(4, 'BSCS', 'Bachelor of Science in Computer Science'),
-(5, 'BSIS', 'Bachelor of Science in Information Systems'),
-(6, 'BSED', 'Bachelor in Secondary Education');
+(1, 'Soft Skills Training', 'Communication Skill, Negotiation Skill, Critical Thinking and Problem-Solving Skills'),
+(4, 'Financial Literacy Training', 'Cash-Flow, Networth, Money in, Money out.'),
+(5, 'Entrepreneurship Training', 'Idea Research, Product Development, Marketing the Product, Cashing out.');
 
 -- --------------------------------------------------------
 
@@ -71,16 +57,19 @@ CREATE TABLE `faculty` (
   `contact` varchar(100) NOT NULL,
   `gender` varchar(100) NOT NULL,
   `address` text NOT NULL,
-  `email` varchar(200) NOT NULL
+  `email` varchar(200) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `lga` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `faculty`
 --
 
-INSERT INTO `faculty` (`id`, `id_no`, `firstname`, `middlename`, `lastname`, `contact`, `gender`, `address`, `email`) VALUES
-(1, '06232014', 'John', 'C', 'Smith', '+18456-5455-55', 'Male', 'Sample Address', 'jsmith@sample.com'),
-(2, '37362629', 'Claire', 'C', 'Blake', '+12345687923', 'Female', 'Sample Address', 'cblake@sample.com');
+INSERT INTO `faculty` (`id`, `id_no`, `firstname`, `middlename`, `lastname`, `contact`, `gender`, `address`, `email`, `state`, `lga`) VALUES
+(7, '64506025', 'Sagir', 'Tanimu', 'Musa', '08022334455', 'Male', 'Gwarzo Road, Rijiyar Zaki, Kano', 'mtsagir@gmail.com', 'Kano', 'Nassarawa'),
+(8, '07549615', 'Jamila', 'Mohammed', 'Salisu', '08028752833', 'Female', 'Unguwan Dosa, Kaduna', 'jamilusalis@gmail.com', 'Kaduna', 'Kaduna North'),
+(9, '71786277', 'Nabeelah', 'Garba', 'Ahmed', '08066664444', 'Female', 'NDA, Kaduna', 'nabeelah@gmail.com', 'Adamawa', 'Yola North');
 
 -- --------------------------------------------------------
 
@@ -91,47 +80,20 @@ INSERT INTO `faculty` (`id`, `id_no`, `firstname`, `middlename`, `lastname`, `co
 CREATE TABLE `schedules` (
   `id` int(30) NOT NULL,
   `faculty_id` int(30) NOT NULL,
-  `title` varchar(200) NOT NULL,
-  `schedule_type` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1= class, 2= meeting,3=others',
-  `description` text NOT NULL,
+  `title` varchar(200) DEFAULT NULL,
+  `state` varchar(50) NOT NULL,
+  `lga` varchar(50) NOT NULL,
   `location` text NOT NULL,
-  `is_repeating` tinyint(1) NOT NULL DEFAULT 1,
-  `repeating_data` text NOT NULL,
+  `contact_person` varchar(200) NOT NULL,
   `schedule_date` date NOT NULL,
   `time_from` time NOT NULL,
   `time_to` time NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+  `description` text NOT NULL,
+  `is_repeating` tinyint(1) NOT NULL DEFAULT '1',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `repeating_data` text,
+  `schedule_type` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `schedules`
---
-
-INSERT INTO `schedules` (`id`, `faculty_id`, `title`, `schedule_type`, `description`, `location`, `is_repeating`, `repeating_data`, `schedule_date`, `time_from`, `time_to`, `date_created`) VALUES
-(3, 2, 'Class 101 (M & Th)', 1, 'Sample Only', 'Online', 1, '{\"dow\":\"1,4\",\"start\":\"2020-10-01\",\"end\":\"2020-11-30\"}', '0000-00-00', '09:00:00', '12:00:00', '2020-10-20 15:51:01');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `subjects`
---
-
-CREATE TABLE `subjects` (
-  `id` int(30) NOT NULL,
-  `subject` varchar(200) NOT NULL,
-  `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `subjects`
---
-
-INSERT INTO `subjects` (`id`, `subject`, `description`) VALUES
-(1, 'DBMS', 'Database Management System'),
-(2, 'Mathematics', 'Mathematics'),
-(3, 'English', 'English'),
-(4, 'Computer Hardware', 'Computer Hardware'),
-(5, 'History', 'History');
 
 -- --------------------------------------------------------
 
@@ -144,7 +106,7 @@ CREATE TABLE `users` (
   `name` text NOT NULL,
   `username` varchar(200) NOT NULL,
   `password` text NOT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT 3 COMMENT '1=Admin,2=Staff, 3= subscriber'
+  `type` tinyint(1) NOT NULL DEFAULT '3' COMMENT '1=Admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -152,17 +114,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
-(1, 'Administrator', 'admin', '0192023a7bbd73250516f069df18b500', 1);
+(1, 'Super Admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `class_schedule_info`
---
-ALTER TABLE `class_schedule_info`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `courses`
@@ -183,12 +139,6 @@ ALTER TABLE `schedules`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `subjects`
---
-ALTER TABLE `subjects`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -197,12 +147,6 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `class_schedule_info`
---
-ALTER TABLE `class_schedule_info`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `courses`
@@ -214,19 +158,13 @@ ALTER TABLE `courses`
 -- AUTO_INCREMENT for table `faculty`
 --
 ALTER TABLE `faculty`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `schedules`
 --
 ALTER TABLE `schedules`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `subjects`
---
-ALTER TABLE `subjects`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
